8081ecb6669a2c74580f16fef7e80b005d6e1f83
Gabriel R.P <0358929@gmail.com>
2025-04-25 09:06:36 -0400

    What i wrote on the test

---
+++ Comparing2StrinngAsEquall.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class Comparing2StrinngAsEquall here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Comparing2StrinngAsEquall
+{
+    // instance variables - replace the example below with your own
+    private int a;
+    private int b;
+    /**
+     * Constructor for objects of class Comparing2StrinngAsEquall
+     */
+    public Comparing2StrinngAsEquall()
+    {
+        // initialise instance variables
+        int a = 0;
+        int b = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        while(!(a==b){
+            a++;
+            System.out.println(a + " & " + b + " are not the same value.");
+        else
+            System.out.println(a + " & " + b + " are the same value.");
+        }
+    }
+}
---
+++ Quesiton_25_26_27.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class Quesiton_25 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Quesiton_25_26_27
+{
+    // instance variables - replace the example below with your own
+
+    /**
+     * An example of a method - replace this comment with your own
+     * Question 25
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public hasFever (ArrayList<tempertaure>()){
+        int isSick = 0;
+        int isNotSick = 0;
+        int Highest = 0;
+        for-each(ArrayList<>()>37.5){
+            isSick++;
+            if(Highest < ArrayList<>()){
+                Highest = ArrayList<>();}
+            }
+        else
+            isNotSick++;}
+        return System.out.println("There are " + isSick + " are sick, and " + isNotSick + 
+                " that are not sick. The highest temperture tested was " + Heighest + " `C.")
+            }
+        
+    }
+}
+/**
+ * question 26
+ * ArrayList<>().Collection.max;
+ * return;
+ * Question 27
+ * ArrayList<>().Collection.sort;
+ * int index.get();
+ * System.out.println(index);
+ */
